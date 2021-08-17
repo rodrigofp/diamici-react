fb2598fb907e6bae6278a2448553f8a5dc9327f6
rodrigo.paulino <rodrigo.paulino@prima.com.br>
2021-08-16 21:34:53 -0300

    Setup dev dependecies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         plugins: [
@@ -37,7 +37,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     };
 };
